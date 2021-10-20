8961fbfdfffde39a3092162310d80effea1f8808
Verwulff <verowulf@gmail.com>
2021-10-20 23:43:31 +0300

    initial commit

---
+++ src/main/java/com/example/upskillingprogram/UpskillingProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.upskillingprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpskillingProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpskillingProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/upskillingprogram/UpskillingProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.upskillingprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpskillingProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
